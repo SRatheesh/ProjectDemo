0c1f3586348ae6817ea118e9931d411898467edf
ratheesh.shivraman <ratheesh.shivraman@Mac002s-Mac-mini.local>
2015-12-29 13:15:31 +0530

    title changed

--- GitProject/GitProject/ViewController.m
+++ GitProject/GitProject/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"Page Number 1";
 }
 
 - (void)didReceiveMemoryWarning {
