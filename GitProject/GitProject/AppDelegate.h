0d92a3b8598c2104e7990af9a8eee04de880070c
ratheesh.shivraman <ratheesh.shivraman@Mac002s-Mac-mini.local>
2015-12-29 12:45:00 +0530

    GitProject file created

---
+++ GitProject/GitProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitProject
+//
+//  Created by ratheesh.shivraman on 29/12/15.
+//  Copyright © 2015 ratheesh.shivraman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitProject/GitProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitProject
+//
+//  Created by ratheesh.shivraman on 29/12/15.
+//  Copyright © 2015 ratheesh.shivraman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
